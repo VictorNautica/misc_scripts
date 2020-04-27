55f7e814071b634f88b8192032b4a1ea68e242c3
Victor Yu <victoryu@fastmail.com>
2020-04-27 20:09:45 +0100

    start

---
+++ hex_bw.R
@@ -0,0 +1,20 @@
+hex_bw <- function(hex_code) {
+  
+  myrgb <- as.integer(col2rgb(hex_code))
+  
+  rgb_convert <- lapply(myrgb, function(x) {
+    i <- x / 255
+    if (i <= 0.03928) {
+      i <- i / 12.92
+    } else {
+      i <- ((i + 0.055) / 1.055) ^ 2.4
+    }
+    return(i)
+  })
+
+ rgb_calc <- (0.2126 * rgb_convert[[1]]) + (0.7152 * rgb_convert[[2]]) + (0.0722 * rgb_convert[[3]])
+ 
+ if (rgb_calc > 0.179) return("#000000") else return("#ffffff")
+   
+}
+
