59d256b0a07e43513e4b4b55a5bd6db94530a93f
Victor Yu <victoryu@fastmail.com>
2020-04-27 20:21:20 +0100

    shorten

--- hex_bw.R
+++ hex_bw.R
@@ -2,7 +2,7 @@ hex_bw <- function(hex_code) {
   
   myrgb <- as.integer(col2rgb(hex_code))
   
-  rgb_convert <- lapply(myrgb, function(x) {
+  rgb_conv <- lapply(myrgb, function(x) {
     i <- x / 255
     if (i <= 0.03928) {
       i <- i / 12.92
@@ -12,7 +12,7 @@ hex_bw <- function(hex_code) {
     return(i)
   })
 
- rgb_calc <- (0.2126 * rgb_convert[[1]]) + (0.7152 * rgb_convert[[2]]) + (0.0722 * rgb_convert[[3]])
+ rgb_calc <- (0.2126*rgb_conv[[1]]) + (0.7152*rgb_conv[[2]]) + (0.0722*rgb_conv[[3]])
  
  if (rgb_calc > 0.179) return("#000000") else return("#ffffff")
    
